9ecd0bfed8d661a069c6c3c4c58136d2a12971c1
jiaying.lu <lujiaying93@foxmail.com>
2015-05-08 16:27:31 +0800

    add bis(), bic() 及衍生的函数

--- bit_operations.cpp
+++ bit_operations.cpp
@@ -1,10 +1,11 @@
 /***
 *Chapter 2 Part I Program Structure and Execution
-*An application of the property that a^a = 0 for any bit vector a.
+*Boolean Algebra
 ***/
 
 #include <stdio.h>
 
+/** An application of the property that a^a = 0 for any bit vector a **/
 void inplace_swap(int *x, int *y) {
 	/* There is no perfomance advantage to this way of swapping */
 	*y = *x ^ *y;
@@ -28,4 +29,25 @@ void reverse_array(int a[], int cnt) {
 		first < last;
 		first++, last--)
 		inplace_swap(&a[first], &a[last]);
+}
+
+
+/** VAX computer's instructions bis(bit set) and bic(bit clear) **/
+/* Declarations of functions implementing operations bis and bic */
+int bis(int x, int m) {
+	return (x | m);
+}
+int bic(int x, int m) {
+	return (x & (~m));
+}
+
+/* Compute x|y using only calls to functions bis and bic */
+int bool_or(int x, int y) {
+	return bis(x, y);
+}
+
+/* Compute x^y using only calls to functions bis and bic */
+int bool_xor(int x, int y) {
+	// x ^ y = (x & ~y) | (~x & y)
+	return bis(bic(x, y), bic(y, x));
 }
